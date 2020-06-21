8831392a3915e5d048ab44e996386d1630494f80
liuxiaoyusky <49766325+liuxiaoyusky@users.noreply.github.com>
2020-06-21 16:20:43 -0500

    play button work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React, {useState} from 'react';
+import React, {Component, useState} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
 //capitalize app can also solve the problem
-const app = (props) => {
+class App extends Component {
   // eslint-disable-next-line react-hooks/rules-of-hooks
-  const [personsState,setPersonsState] = useState({
+  state = {
     persons:[
       {name: 'Max', age: 28},
       {name: 'Menu', age: 29},
       {name: 'Stephanie', age: 26},
     ],
-        otherStates: "some other value"
-  });
+      otherStates: "some other value",
+      showPersons: false
+  };
 
   //function inside function
-  const switchNameHandler = (newName) => {
+  switchNameHandler = (newName) => {
   // console.log("was clicked");
-    setPersonsState({
+    this.setState({
       persons: [
         {name:newName,age:38},
         {name:'Menu',age:29},
@@ -26,46 +27,57 @@ const app = (props) => {
     })
   };
 
-  const styleStyle = {
-      backgroundColor:'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-  };
+   nameChangeHandler = (event) => {
+       this.setState({
+           persons: [
+               {name: 'Max', age: 38},
+               {name: event.target.value, age: 29},
+               {name: 'Stephanie', age: 27},
+           ]
+       })
+   }
 
-  const nameChangeHandler = (event) => {
-      setPersonsState({
-          persons: [
-              {name:'Max',age:38},
-              {name:event.target.value,age:29},
-              {name:'Stephanie',age:27},
-          ]
-      })
+  togglePersonsHandler = () => {
+       const doesShow = this.state.showPersons;
+       this.setState({showPersons:!doesShow});
   }
 
+    render() {
+        const styleStyle = {
+            backgroundColor:'white',
+            font: 'inherit',
+            border: '1px solid blue',
+            padding: '8px',
+            cursor: 'pointer'
+        };
 
-    return (
+       return (
         <div className="App">
             <h1>Hi, I'm a Re0act App.</h1>
             <p>This is really working!</p>
             <button
-                onClick={switchNameHandler.bind(this,"Maximilian")}
-                style={styleStyle}
-            >Switch Name</button>
-            <Person
-                name={personsState.persons[0].name}
-                age={personsState.persons[0].age}>My Habbies: Racing</Person>
-            <Person
-                name={personsState.persons[1].name}
-                age={personsState.persons[1].age}
-                click={() => switchNameHandler("Max!!")}
-                changedd={nameChangeHandler}
-            />
-            <Person
-                name={personsState.persons[2].name}
-                age={personsState.persons[2].age}/>
+                onClick={this.togglePersonsHandler}
+                style={styleStyle}>Switch Name</button>
+            {this.state.showPersons ?
+                <div>
+                <Person
+                    name={this.state.persons[0].name}
+                    age={this.state.persons[0].age}>My Habbies: Racing</Person>
+                <Person
+                    name={this.state.persons[1].name}
+                    age={this.state.persons[1].age}
+                    click={this.switchNameHandler.bind(this,"Max!!")}
+                    changedd={this.nameChangeHandler}
+                />
+                <Person
+                    name={this.state.persons[2].name}
+                    age={this.state.persons[2].age}/>
+            </div> : null
+            }
         </div>
     );
+    }
+
+
 }
-export default app;
+export default App;
