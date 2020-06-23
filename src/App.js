66c2f7753ddb6f369eb3472f2cd69b2057cc76a9
liuxiaoyusky <49766325+liuxiaoyusky@users.noreply.github.com>
2020-06-23 13:40:28 -0500

    69

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
   };
 
   deletePersonHandler  = (personIndex) => {
+
       const newPersons = [...this.state.persons];
       newPersons.splice(personIndex,1);
       this.setState({persons: newPersons});
